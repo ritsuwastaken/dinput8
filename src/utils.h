f39eed6634ccd0a0a4900ddd2b8034b4fed51ddd
ritsu <discohijackat125ruemontmarte@proton.me>
2024-07-29 23:56:23 +0100

    Update header

--- src/utils.h
+++ src/utils.h
@@ -1,47 +1,13 @@
 #ifndef UTILS_H
 #define UTILS_H
 #include <windows.h>
-#include <algorithm>
-#include <cwctype>
 #include <string>
 
 namespace utils
 {
-    __declspec(noinline) LPVOID __cdecl GetReturnAddress()
-    {
-        __asm
-        {
-            MOV EAX, DWORD PTR SS:[EBP + 4]
-        }
-    }
-
-    inline BOOL IsAddressInModule(LPVOID address, const char *moduleName)
-    {
-        MEMORY_BASIC_INFORMATION mbi;
-        char module[MAX_PATH];
-
-        if (VirtualQuery(address, &mbi, sizeof(mbi)) &&
-            GetModuleFileNameA((HMODULE)mbi.AllocationBase, module, sizeof(module)) != 0)
-        {
-            std::string modulePath(module);
-            size_t pos = modulePath.find_last_of("\\/");
-            std::string fileName = (pos != std::string::npos) ? modulePath.substr(pos + 1) : modulePath;
-            return _stricmp(fileName.c_str(), moduleName) == 0;
-        }
-
-        return FALSE;
-    }
-
-    inline bool Contains(const std::wstring &str, const std::wstring &substr)
-    {
-        return std::search(
-                   str.begin(), str.end(),
-                   substr.begin(), substr.end(),
-                   [](wchar_t a, wchar_t b)
-                   {
-                       return std::towlower(a) == std::towlower(b);
-                   }) != str.end();
-    }
+    __declspec(noinline) LPVOID __cdecl GetReturnAddress();
+    inline BOOL IsAddressInModule(LPVOID address, const char *moduleName);
+    inline bool Contains(const std::wstring &str, const std::wstring &substr);
 }
 
 #endif // UTILS_H
