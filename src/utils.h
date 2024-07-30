f20395ccbad8e6b4533b5d9fb87f3bbfdc4caf96
ritsu <discohijackat125ruemontmarte@proton.me>
2024-07-30 10:39:13 +0100

    Update header

--- src/utils.h
+++ src/utils.h
@@ -6,8 +6,8 @@
 namespace utils
 {
     __declspec(noinline) LPVOID __cdecl GetReturnAddress();
-    inline BOOL IsAddressInModule(LPVOID address, const char *moduleName);
-    inline bool Contains(const std::wstring &str, const std::wstring &substr);
+    BOOL IsAddressInModule(LPVOID address, const char *moduleName);
+    bool Contains(const std::wstring &str, const std::wstring &substr);
 }
 
 #endif // UTILS_H
